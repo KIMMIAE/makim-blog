556e22374721abddaec53e12ac6ed442730b3e46
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2022-04-04 20:26:36 +0900

    implemented LayoutWrapper

---
+++ makim-blog/src/components/LayoutWrapper.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Header from "./header";
+
+const LayoutWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default LayoutWrapper;
