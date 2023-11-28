10ac6cfbd438bfc094ae9d704cd4f0e411600cd0
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2023-11-28 23:22:37 +0900

    feat: add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,41 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../public/applejam_logo.png";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" passHref>
-          {/* TODO: 블로그 로고 추가*/}
-          <span className="text-2xl font-semibold cursor-pointer">개발이 재밌는 날</span>
+          <div className="flex items-center">
+            <Image
+              src={logo}
+              width={64}
+              height={64}
+              placeholder="blur"
+              className="w-16 h-16"
+              alt="logo"
+            />
+            <span className="text-3xl font-semibold cursor-pointer">
+              개발이 재밌는 날
+            </span>
+          </div>
         </Link>
       </div>
       <div className="flex items-center">
-        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">Posts</a>
-        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">Tags</a>
-        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">About</a>
+        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">
+          Posts
+        </a>
+        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">
+          Tags
+        </a>
+        <a href="#" className="p-1 font-medium sm:p-4 cursor-pointer">
+          About
+        </a>
         {/* 다크모드 버튼 추가 */}
       </div>
     </header>
   );
-}
+};
 
 export default Header;
