b64d20df80c70a7c76e4a759387b60d72f7d85e4
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2024-01-10 00:02:59 +0900

    refactor: Migrating index.tsx to page.tsx and Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,40 @@
 import "../../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import LayoutWrapper from "../components/LayoutWrapper";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'applejam',
+  description: 'https://applejam.monster',
+  authors: [{ name: 'makim' }],
+  referrer: 'origin-when-cross-origin',
+  creator: 'makim',
+  publisher: 'makim',
+  metadataBase: new URL('https://applejam.monster'),
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  icons: {
+    icon: '/favicon/apple-icon.png',
+    shortcut: '/favicon/apple-icon.png',
+    apple: '/favicon/apple-icon.png',
+    other: {
+      rel: '/favicon/apple-icon-precomposed',
+      url: '/favicon/apple-icon-precomposed.png',
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+}
+
 
 export default function RootLayout({
   children,
