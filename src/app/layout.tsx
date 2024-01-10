88f5b960e59725f11ee653144dcbcaf42648f31f
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2024-01-10 10:58:23 +0900

    refactor: Migrating pages and routing hooks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "../../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 import LayoutWrapper from "../components/LayoutWrapper";
 import { Metadata } from "next";
 import { Providers } from "../components/Provider";
